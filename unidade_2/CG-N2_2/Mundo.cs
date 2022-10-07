031433ced11278cef0d601b2b2850691bb4a371c
Thomas Rodrigues <thomas.rodrigues@smarketsolution.com.br>
2022-10-07 19:58:39 -0300

    atividade 2

--- unidade_2/CG-N2_2/Mundo.cs
+++ unidade_2/CG-N2_2/Mundo.cs
@@ -17,52 +17,52 @@ using CG_Biblioteca;
 
 namespace gcgcg
 {
-  class Mundo : GameWindow
-  {
-    private static Mundo instanciaMundo = null;
-
-    private Mundo(int width, int height) : base(width, height) { }
-
-    public static Mundo GetInstance(int width, int height)
+    class Mundo : GameWindow
     {
-      if (instanciaMundo == null)
-        instanciaMundo = new Mundo(width, height);
-      return instanciaMundo;
-    }
+        private static Mundo instanciaMundo = null;
 
-    private CameraOrtho camera = new CameraOrtho();
-    protected List<Objeto> objetosLista = new List<Objeto>();
-    private ObjetoGeometria objetoSelecionado = null;
-    private char objetoId = '@';
-    private bool bBoxDesenhar = false;
-    int mouseX, mouseY;   //TODO: achar método MouseDown para não ter variável Global
-    private bool mouseMoverPto = false;
-    // private Retangulo obj_Retangulo;
-    private Circulo obj_Circulo;
+        private Mundo(int width, int height) : base(width, height) { }
+
+        public static Mundo GetInstance(int width, int height)
+        {
+            if (instanciaMundo == null)
+                instanciaMundo = new Mundo(width, height);
+            return instanciaMundo;
+        }
+
+        private CameraOrtho camera = new CameraOrtho();
+        protected List<Objeto> objetosLista = new List<Objeto>();
+        private ObjetoGeometria objetoSelecionado = null;
+        private char objetoId = '@';
+        private bool bBoxDesenhar = false;
+        int mouseX, mouseY;   //TODO: achar método MouseDown para não ter variável Global
+        private bool mouseMoverPto = false;
+        // private Retangulo obj_Retangulo;
+        private Circulo obj_Circulo;
 #if CG_Privado
     private Privado_SegReta obj_SegReta;
     private Privado_Circulo obj_Circulo;
 #endif
 
-    protected override void OnLoad(EventArgs e)
-    {
-      base.OnLoad(e);
-      camera.xmin = -300; camera.xmax = 300; camera.ymin = -300; camera.ymax = 300;
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            camera.xmin = -300; camera.xmax = 300; camera.ymin = -300; camera.ymax = 300;
 
-      Console.WriteLine(" --- Ajuda / Teclas: ");
-      Console.WriteLine(" [  H     ] mostra teclas usadas. ");
+            Console.WriteLine(" --- Ajuda / Teclas: ");
+            Console.WriteLine(" [  H     ] mostra teclas usadas. ");
 
-      objetoId = Utilitario.charProximo(objetoId);
-      // obj_Retangulo = new Retangulo(objetoId, null, new Ponto4D(50, 50, 0), new Ponto4D(150, 150, 0));
-      // obj_Retangulo.ObjetoCor.CorR = 255; obj_Retangulo.ObjetoCor.CorG = 0; obj_Retangulo.ObjetoCor.CorB = 255;
-      // objetosLista.Add(obj_Retangulo);
-      // objetoSelecionado = obj_Retangulo;
-      obj_Circulo = new Circulo(objetoId, null, new Ponto4D(0, 0, 0), 100, 72);
-      obj_Circulo.ObjetoCor.CorR = 0; obj_Circulo.ObjetoCor.CorG = 0; obj_Circulo.ObjetoCor.CorB = 0;
-      obj_Circulo.PrimitivaTipo = PrimitiveType.Points;
-      obj_Circulo.PrimitivaTamanho = 5;
-      objetosLista.Add(obj_Circulo);
-      objetoSelecionado = obj_Circulo;
+            objetoId = Utilitario.charProximo(objetoId);
+            // obj_Retangulo = new Retangulo(objetoId, null, new Ponto4D(50, 50, 0), new Ponto4D(150, 150, 0));
+            // obj_Retangulo.ObjetoCor.CorR = 255; obj_Retangulo.ObjetoCor.CorG = 0; obj_Retangulo.ObjetoCor.CorB = 255;
+            // objetosLista.Add(obj_Retangulo);
+            // objetoSelecionado = obj_Retangulo;
+            obj_Circulo = new Circulo(objetoId, null, new Ponto4D(0, 0, 0), 100, 72);
+            obj_Circulo.ObjetoCor.CorR = 0; obj_Circulo.ObjetoCor.CorG = 0; obj_Circulo.ObjetoCor.CorB = 0;
+            obj_Circulo.PrimitivaTipo = PrimitiveType.Points;
+            obj_Circulo.PrimitivaTamanho = 5;
+            objetosLista.Add(obj_Circulo);
+            objetoSelecionado = obj_Circulo;
 
 #if CG_Privado
       objetoId = Utilitario.charProximo(objetoId);
@@ -80,109 +80,110 @@ namespace gcgcg
       objetoSelecionado = obj_Circulo;
 #endif
 #if CG_OpenGL
-      GL.ClearColor(255, 255, 255, 255);
+            GL.ClearColor(255, 255, 255, 255);
 #endif
-    }
-    protected override void OnUpdateFrame(FrameEventArgs e)
-    {
-      base.OnUpdateFrame(e);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
 #if CG_OpenGL
-      GL.MatrixMode(MatrixMode.Projection);
-      GL.LoadIdentity();
-      GL.Ortho(camera.xmin, camera.xmax, camera.ymin, camera.ymax, camera.zmin, camera.zmax);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(camera.xmin, camera.xmax, camera.ymin, camera.ymax, camera.zmin, camera.zmax);
 #endif
-    }
-    protected override void OnRenderFrame(FrameEventArgs e)
-    {
-      base.OnRenderFrame(e);
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
 #if CG_OpenGL
-      GL.Clear(ClearBufferMask.ColorBufferBit);
-      GL.MatrixMode(MatrixMode.Modelview);
-      GL.LoadIdentity();
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
 #endif
-#if CG_Gizmo      
-      Sru3D();
-#endif
-      for (var i = 0; i < objetosLista.Count; i++)
-        objetosLista[i].Desenhar();
 #if CG_Gizmo
-      if (bBoxDesenhar && (objetoSelecionado != null))
-        objetoSelecionado.BBox.Desenhar();
+            Sru3D();
 #endif
-      this.SwapBuffers();
-    }
+            for (var i = 0; i < objetosLista.Count; i++)
+                objetosLista[i].Desenhar();
+#if CG_Gizmo
+            if (bBoxDesenhar && (objetoSelecionado != null))
+                objetoSelecionado.BBox.Desenhar();
+#endif
+            this.SwapBuffers();
+        }
 
-    protected override void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
-    {
-      if (e.Key == Key.H)
-        Utilitario.AjudaTeclado();
-      else if (e.Key == Key.Escape)
-        Exit();
-      else if (e.Key == Key.V)
-        mouseMoverPto = !mouseMoverPto;   //TODO: falta atualizar a BBox do objeto
-      
-      else if (e.Key == Key.E && camera.xmax <= 600)
-        camera.PanDireita();
+        protected override void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.H)
+                Utilitario.AjudaTeclado();
+            else if (e.Key == Key.Escape)
+                Exit();
+            else if (e.Key == Key.V)
+                mouseMoverPto = !mouseMoverPto;   //TODO: falta atualizar a BBox do objeto
 
-      else if (e.Key == Key.D && camera.xmin >= -600)
-        camera.PanEsquerda();
+            else if (e.Key == Key.E && camera.xmin >= -600)
+                camera.PanDireita();
 
-      else if (e.Key == Key.C && camera.ymin >= -600)
-        camera.PanBaixo();
+            else if (e.Key == Key.D && camera.xmax <= 600)
+                camera.PanEsquerda();
 
-      else if (e.Key == Key.B && camera.ymax <= 600)
-        camera.PanCima();
+            else if (e.Key == Key.C && camera.ymax <= 600)
+                camera.PanBaixo();
 
-      else if (e.Key == Key.I && camera.xmin < -100 && camera.ymin < -100)
-        camera.ZoomIn();
+            else if (e.Key == Key.B && camera.ymin >= -600)
+                camera.PanCima();
 
-      else if (e.Key == Key.O && camera.xmin > -400 && camera.ymin > -400) {
-        Console.WriteLine("xmin: " + camera.xmin);
-        Console.WriteLine("ymin: " + camera.ymin);
-        camera.ZoomOut();
-      }
-      else
-        Console.WriteLine(" __ Tecla não implementada.");
-    }
+            else if (e.Key == Key.I && camera.xmin < -100 && camera.ymin < -100)
+                camera.ZoomIn();
 
-    //TODO: não está considerando o NDC
-    protected override void OnMouseMove(MouseMoveEventArgs e)
-    {
-      mouseX = e.Position.X; mouseY = 600 - e.Position.Y; // Inverti eixo Y
-      if (mouseMoverPto && (objetoSelecionado != null))
-      {
-        objetoSelecionado.PontosUltimo().X = mouseX;
-        objetoSelecionado.PontosUltimo().Y = mouseY;
-      }
-    }
+            else if (e.Key == Key.O && camera.xmin > -400 && camera.ymin > -400)
+            {
+                Console.WriteLine("xmin: " + camera.xmin);
+                Console.WriteLine("ymin: " + camera.ymin);
+                camera.ZoomOut();
+            }
+            else
+                Console.WriteLine(" __ Tecla não implementada.");
+        }
+
+        //TODO: não está considerando o NDC
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            mouseX = e.Position.X; mouseY = 600 - e.Position.Y; // Inverti eixo Y
+            if (mouseMoverPto && (objetoSelecionado != null))
+            {
+                objetoSelecionado.PontosUltimo().X = mouseX;
+                objetoSelecionado.PontosUltimo().Y = mouseY;
+            }
+        }
 
 #if CG_Gizmo
-    private void Sru3D()
-    {
+        private void Sru3D()
+        {
 #if CG_OpenGL
-      GL.LineWidth(1);
-      GL.Begin(PrimitiveType.Lines);
-      // GL.Color3(1.0f,0.0f,0.0f);
-      GL.Color3(Convert.ToByte(255), Convert.ToByte(0), Convert.ToByte(0));
-      GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
-      // GL.Color3(0.0f,1.0f,0.0f);
-      GL.Color3(Convert.ToByte(0), Convert.ToByte(255), Convert.ToByte(0));
-      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
-      // GL.Color3(0.0f,0.0f,1.0f);
-      GL.Color3(Convert.ToByte(0), Convert.ToByte(0), Convert.ToByte(255));
-      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
-      GL.End();
+            GL.LineWidth(1);
+            GL.Begin(PrimitiveType.Lines);
+            // GL.Color3(1.0f,0.0f,0.0f);
+            GL.Color3(Convert.ToByte(255), Convert.ToByte(0), Convert.ToByte(0));
+            GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
+            // GL.Color3(0.0f,1.0f,0.0f);
+            GL.Color3(Convert.ToByte(0), Convert.ToByte(255), Convert.ToByte(0));
+            GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
+            // GL.Color3(0.0f,0.0f,1.0f);
+            GL.Color3(Convert.ToByte(0), Convert.ToByte(0), Convert.ToByte(255));
+            GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
+            GL.End();
+#endif
+        }
 #endif
     }
-#endif    
-  }
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Mundo window = Mundo.GetInstance(600, 600);
-      window.Title = "CG_N2";
-      window.Run(1.0 / 60.0);
+        static void Main(string[] args)
+        {
+            Mundo window = Mundo.GetInstance(600, 600);
+            window.Title = "CG_N2";
+            window.Run(1.0 / 60.0);
+        }
     }
-  }
 }
